753ff5e3420ff200066605344236dbe1f65d81b6
mulin.lyh <mulin.lyh@taobao.com>
2024-06-03 18:42:19 +0800

    Fixed broken of download models from modelscope

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -73,6 +73,26 @@ def test_model_from_modelscope(monkeypatch):
     finally:
         monkeypatch.delenv("VLLM_USE_MODELSCOPE", raising=False)
 
+def test_model_from_modelscope(monkeypatch):
+    # model: https://modelscope.cn/models/qwen/Qwen1.5-0.5B-Chat/summary
+    MODELSCOPE_MODEL_NAME = "qwen/Qwen1.5-0.5B-Chat"
+    monkeypatch.setenv("VLLM_USE_MODELSCOPE", "True")
+    try:
+        llm = LLM(model=MODELSCOPE_MODEL_NAME)
+
+        prompts = [
+            "Hello, my name is",
+            "The president of the United States is",
+            "The capital of France is",
+            "The future of AI is",
+        ]
+        sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+
+        outputs = llm.generate(prompts, sampling_params)
+        assert len(outputs) == 4
+    finally:
+        monkeypatch.delenv("VLLM_USE_MODELSCOPE", raising=False)
+
 
 if __name__ == "__main__":
     import pytest
